a6450789f317ae415ec2ce3ae9be68a13d5dd10a
Steven Tschache <github@tschache.com>
2021-01-20 00:45:43 +0000

    fixed after wrong commit

--- terraform/modules/aws-certificate/main.tf
+++ terraform/modules/aws-certificate/main.tf
@@ -1,6 +1,7 @@
 
 # create AWS resource of that certificate
 resource "aws_acm_certificate" "this" {
+#resource "aws_lb_listener_certificate" "this" {
   private_key      = var.private_key_pem
   certificate_body = var.cert_pem
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,15 +10,37 @@ output vpc_arns {
 
 output instance_ids {
   description = "IDs of EC2 instances"
-  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].instance_ids }
+  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].instance_id }
 }
 
 output public_instance_ips {
   description = "Public IPs of the instance for each project"
-  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].public_ips }
+  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].public_ip }
 }
 
 output public_instance_dns {
   description = "Public DNSs of the instance for each project"
   value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].public_dns }
 }
+
+variable "inventory_path" {
+    default = "../ansible/inventory/inventory.ini"
+}
+
+resource "local_file" "config" {
+    content = templatefile("./ansible_config.tmpl", {
+	private_key 	= var.private_key_path,
+	ssh_user	= var.ssh_instance_user,
+	inventory_path  = var.inventory_path
+    })
+    filename = "../ansible/ansible.cfg"
+}
+
+resource "local_file" "inventory" {
+    content = templatefile("./ansible_inventory.tmpl", {
+	elb 	= module.elb_http,
+	http 	= module.ec2_instances
+    })
+    filename = var.inventory_path
+}
+
